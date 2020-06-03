c9a2a5f0055d63304df765b427c8770fd65db7df
jafoor <abujafor.cse11@gmai.com>
2020-06-03 14:19:19 +0600

    final

--- covid19/covid19/settings.py
+++ covid19/covid19/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '-(cokn)ow64cyj*a7b3(smdh+rv2r-w$*fjqja99i^-h61a7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
